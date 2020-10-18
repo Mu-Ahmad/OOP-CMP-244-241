e6bc23d9f7873906d7d86d94ba27a1210b9b4ac9
iSaadi <72272685+saadiPro@users.noreply.github.com>
2020-10-18 12:38:29 +0500

    Update ResursiveFunction.cpp

--- Extras/ResursiveFunction.cpp
+++ Extras/ResursiveFunction.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int factorial(int n) {
-    if (n > 1) return n * factorial(n - 1);
-}int main() {
-    int n, result;
-    cout << "Enter a number: ";
-    cin >> n;
-    result = (n==1) ? 1: factorial(n);
-    cout << result;
-    return 0;
+          if (n > 1) return n * factorial(n - 1);
+}
+
+int main() {
+            int n, result;
+            cout << "Enter a number: ";
+            cin >> n;
+            result = (n==1) ? 1: factorial(n);
+            cout << result;
+            return 0;
 }
