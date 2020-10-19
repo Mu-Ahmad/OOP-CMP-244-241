58a2ed015f8e92ea0c6339b5a5d8d580a45108ba
iSaadi <72272685+saadiPro@users.noreply.github.com>
2020-10-19 17:27:01 +0500

    Update ResursiveFunction.cpp

--- Extras/ResursiveFunction.cpp
+++ Extras/ResursiveFunction.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 
 long double factorial(int n) {
-          if (n <= 1) // base case
-        return 1;
-    else    
-        return n*factorial(n-1);   
+         return (n <= 1) ? 1 : n*factorial(n-1); 
 }
 
 int main() {
