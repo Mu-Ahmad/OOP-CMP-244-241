297eb5dfcece71ab632bbc523a2293557471b955
Muhammad Ahmad <ahmadsaifofficial@gmail.com>
2021-03-15 01:17:53 +0500

    Add Solution to Lab 4

---
+++ DSA/LAB/Lab 3/task1(509).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
+// O(n) Approach using running sum technique
+int solve(int* runningSum, int n, int curr, int ans, int globalMin) {
+  // Base Case
+  if (curr == n)  return ans;
+
+  // Recursive Case
+  int diff = abs(runningSum[n] - 2 * runningSum[curr]);
+  if (diff < globalMin) {
+    ans = curr;
+    globalMin = diff;
+  }
+  return solve(runningSum, n, curr + 1, ans, globalMin);
+}
+
+int main() {
+  int n; cin >> n;
+  int arr[n];
+  int runningSum[n + 1] = {0} ;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    runningSum[i + 1] = arr[i] + runningSum[i];
+  }
+
+  // O(n^2) approach
+  int ans = 0; int globalMin = 100000;
+  for (int i = 0; i < n; i++) { // O(n)
+    int group1 = accumulate(arr, arr + i, 0);   // O(n)
+    int group2 = accumulate(arr + i, arr + n, 0); // O(n)
+    int diff = abs(group1 - group2);
+    if (diff < globalMin) {
+      ans = i;
+      globalMin = diff;
+    }
+  }
+
+  cout << ans << '\n';
+
+  // O(n) approach
+  cout << solve(runningSum, n, 1, 0, 10000) << '\n';
+}
---
+++ DSA/LAB/Lab 3/task2(509).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <bitset>
+
+using namespace std;
+
+bitset<1000> Isprime;
+void primeSieve(bitset<1000>& arr, int n) {
+	for (int i = 3; i <= n; i += 2) {
+		arr[i] = 1;
+	}
+	for (long long i = 3; i <= n; i += 2) {
+		if (arr[i]) {
+			for (long long j = i * i; j < n; j += i) {
+				arr[j] = 0;
+			}
+		}
+	}
+	arr[0] = arr[1] = 0;
+	arr[2] = 1;
+}
+
+// Helper function to grab next prime, given a number
+int nextPrime(int curr) {
+	for (int i = curr + 1; i < 10000001; i++)
+		if (Isprime[i]) return i;
+
+	return -1;
+}
+
+// Helper log function
+double log0f(int val, int base) {
+	return log(val) / log(base);
+}
+
+void Magic(int n, int prime) {
+	// Base Case
+	if (n < prime) {
+		(n) ? cout << n << '\n' : cout << '\n';
+		return;
+	}
+	// Recursive Case
+	int exponent = floor(log0f(n, prime));
+	cout << prime << ' ' << exponent << ' ';
+	return Magic(n - (int)pow(prime, exponent), nextPrime(prime));
+}
+
+int main() {
+	primeSieve(Isprime, 1000); // initiate the list of primes in first 1000 natural numbers
+	int n; cin >> n;
+	Magic(n, 2);
+	return 0;
+}
